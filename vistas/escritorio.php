307f3ef8fee749ced38a242d994b93d21c3c80c6
palmachris7 <palmachristopher7@hotmail.com>
2022-05-11 19:04:49 -0500

    cambios

--- vistas/escritorio.php
+++ vistas/escritorio.php
@@ -1,5 +1,6 @@
-<?php
+ <?php
 //Activamos el almacenamiento en el buffer
+
 ob_start();
 session_start();
 
@@ -24,32 +25,6 @@ if ($_SESSION['escritorio']==1)
   $totalv=$regv->total_venta;
 
   $rsptau = $consulta->totalventahoy();
-
-  //Datos para mostrar el gráfico de barras de las compras
-  $compras10 = $consulta->comprasultimos_10dias();
-  $fechasc='';
-  $totalesc='';
-  while ($regfechac= $compras10->fetch_object()) {
-    $fechasc=$fechasc.'"'.$regfechac->fecha .'",';
-    $totalesc=$totalesc.$regfechac->total .','; 
-  }
-
-  //Quitamos la última coma
-  $fechasc=substr($fechasc, 0, -1);
-  $totalesc=substr($totalesc, 0, -1);
-
-   //Datos para mostrar el gráfico de barras de las ventas
-  $ventas12 = $consulta->ventasultimos_12meses();
-  $fechasv='';
-  $totalesv='';
-  while ($regfechav= $ventas12->fetch_object()) {
-    $fechasv=$fechasv.'"'.$regfechav->fecha .'",';
-    $totalesv=$totalesv.$regfechav->total .','; 
-  }
-
-
-  $fechasv=substr($fechasv, 0, -1);
-  $totalesv=substr($totalesv, 0, -1);
 
 ?>
 <!--Contenido-->
@@ -87,61 +62,12 @@ if ($_SESSION['escritorio']==1)
                             </div>
                         </div>
                    
-                   
-                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
-                          <div class="small-box bg-aqua">
-                              <div class="inner">
-                                <h4 style="font-size:17px;">
-                                <i class="fa-solid fa-minus fa-2xl"></i> <strong>S/ <?php echo $totalc; ?></strong>
-                                </h4>
-                                
-                                <p>Compras de hoy</p>
-                              </div>
-                              <div class="icon">
-                                <i class="ion ion-bag"></i>
-                              </div>
-                              <a href="ingreso.php" class="small-box-footer"> Compras <i class="fa fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
 
-                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-6">
-                          <div class="small-box bg-green">
-                              <div class="inner">
-                                <h4 style="font-size:17px;">
-                                <i class="fa-solid fa-plus fa-2xl"></i> <strong>S/ <?php echo $totalv; ?></strong>
-                                </h4>
-                                <p>Ventas de hoy</p>
-                              </div>
-                              <div class="icon">
-                                <i class="ion ion-bag"></i>
-                              </div>
-                              <a href="venta.php" class="small-box-footer"> Ventas <i class="fa fa-arrow-circle-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="panel-body">
-                        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
-                          <div class="box box-primary">
-                              <div class="box-header with-border">
-                                Compras de los últimos 10 días
-                              </div>
-                              <div class="box-body">
-                                <canvas id="compras" width="400" height="300"></canvas>
-                              </div>
-                          </div>
-                        </div>
-                        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
-                          <div class="box box-primary">
-                              <div class="box-header with-border">
-                                Ventas de los últimos 12 meses
-                              </div>
-                              <div class="box-body">
-                                <canvas id="ventas" width="400" height="300"></canvas>
-                              </div>
-                          </div>
-                        </div>
-                    </div>
+
+                     
+
+
+
                     <!--Fin centro -->
                   </div><!-- /.box -->
               </div><!-- /.col -->
@@ -162,100 +88,6 @@ require 'footer.php';
 
 <script src="../public/js/chart.min.js"></script>
 <script src="../public/js/Chart.bundle.min.js"></script> 
-<script type="text/javascript">
-var ctx = document.getElementById("compras").getContext('2d');
-var compras = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [<?php echo $fechasc; ?>],
-        datasets: [{
-            label: 'Compras en S/ de los últimos 10 días',
-            data: [<?php echo $totalesc; ?>],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-
-var ctx = document.getElementById("ventas").getContext('2d');
-var ventas = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [<?php echo $fechasv; ?>],
-        datasets: [{
-            label: 'Ventas en S/ de los últimos 12 Meses',
-            data: [<?php echo $totalesv; ?>],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-</script>
-
 
 </script>
 
