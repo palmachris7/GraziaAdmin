ff4accf6765cb984035a9627194c1c8d7c1d7d00
palmachris7 <palmachristopher7@hotmail.com>
2022-10-07 21:53:22 -0500

    Nueva version

--- api/conexion.php
+++ api/conexion.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 $mysql=new mysqli("localhost","root","root","grazia");
 if($mysql->connect_error){
    die("Error de conexion");
@@ -8,10 +8,5 @@ else{
    echo "Conexion exitosa";
 }
 
-*/
-$i0 = new mysqli(base64_decode('bG9jYWxob3N0'), base64_decode('cm9vdA=='), base64_decode('cm9vdA=='), base64_decode('Z3Jhemlh'));
-if ($i0->connect_error) {
-    die(base64_decode('RXJyb3IgZGUgY29uZXhpb24='));
-} else {
-    echo base64_decode('Q29uZXhpb24gZXhpdG9zYQ==');
-} ?>
+
+ ?>
--- config/global.php
+++ config/global.php
@@ -1,5 +1,5 @@
 <?php
-/* / /Ip de la pc servidor de base de datos
+//Ip de la pc servidor de base de datos
 define("DB_HOST","localhost");
 
 //Nombre de la base de datos
@@ -16,12 +16,5 @@ define("DB_ENCODE","utf8");
 
 // constante como nombre del proyecto
 define("PRO_NOMBRE","Grazia");
-?>*/
+?>
 
-
-define(base64_decode('REJfSE9TVA=='), base64_decode('bG9jYWxob3N0'));
-define(base64_decode('REJfTkFNRQ=='), base64_decode('Z3Jhemlh'));
-define(base64_decode('REJfVVNFUk5BTUU='), base64_decode('cm9vdA=='));
-define(base64_decode('REJfUEFTU1dPUkQ='), base64_decode('cm9vdA=='));
-define(base64_decode('REJfRU5DT0RF'), base64_decode('dXRmOA=='));
-define(base64_decode('UFJPX05PTUJSRQ=='), base64_decode('R3Jhemlh')); ?>
